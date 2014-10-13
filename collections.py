dea401ffb749a943ca2192e71e1968ea2fad5843
Elder Santos <elder.santos@gmail.com>
2014-10-12 23:23:44 -0300

    collections

---
+++ collections.py
@@ -0,0 +1,31 @@
+''' tuples '''
+import math
+
+t = ("First Element", 2, 3.14, (1,'Nested Yeah',3))
+print("First item: ", t[0])
+print("Second item: ", t[1])
+print("Thirth item: ", t[2])
+print("Nested item: ", t[3][1])
+print('The last element of a tuple = {0}'.format(t[-1]))
+
+s = ''.join(['Elder', 'Santos', 'Foda'])
+print(s)
+
+'''
+h = ()
+print(type(h))
+print(len(h))
+h = 9, 15
+print(type(h))
+print(2 in t)
+'''
+
+p = s.partition('Santos')
+print(p)
+
+f = 'The age og {0} is {1} that is really {1}'.format('Elder', 31)
+print(f)
+g = 'The values of the first tuple is one={t[0]} two={t[1]} and three={t[2]}'.format(t=t)
+print(g)
+print('Math contants pi({m.pi:.2f}) and e=({m.e:.4f})'.format(m=math))
+
